0ee6da04aa64df660d695924e5e4407b2b8c4257
AtharvaWadodkar <115360073+AtharvaWadodkar@users.noreply.github.com>
2025-03-20 11:53:30 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
-import java.sql.DriverManager
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
